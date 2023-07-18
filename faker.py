09673121f518fa4310741a2e12eacaa2eab686fd
Jonathan Opperman <opperman.jonathan@gmail.com>
2023-07-18 12:08:49 +0200

    Update faker.py
    Updated to export to json file

--- faker.py
+++ faker.py
@@ -1,26 +1,25 @@
-
 from faker import Faker
 import json
 
-#Intiaize Faker
+# Initialize Faker
 fake = Faker()
 
-fake.name()
-'John'
-fake.last_name()
-'Doe'
-fake.country()
-'South Africa'
-fake.text()
-'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.'
-fake.email()
-'myfakemail@example.org'
-fake.date()
-'1985-01-06'
-fake.phone_number()
-'081 6054545'
+data = []
 
-#Loop through range up to 100
+# Loop through range up to 100
 for val in range(100):
-    y= json.dumps((f"Name: {fake.name()} Lastname: {fake.last_name()} PhoneNumber: {fake.phone_number()} Country: {fake.country()} Text: {fake.text()} Date: {fake.date()}"))
-    print(y)
+    item = {
+        "Name": fake.name(),
+        "Lastname": fake.last_name(),
+        "PhoneNumber": fake.phone_number(),
+        "Country": fake.country(),
+        "Text": fake.text(),
+        "Date": fake.date()
+    }
+    data.append(item)
+
+# Write data to JSON file
+with open("output.json", "w") as f:
+    json.dump(data, f, indent=4)
+
+print("Data written to output.json file.")
